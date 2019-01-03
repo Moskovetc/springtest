773124e1762fae744df7e1bbabc0b8e24d9d13a3
Leonid <moskovetc@hotmail.com>
2019-01-03 13:40:58 +0400

    add lesson 6

--- src/main/java/CacheFileEventLogger.java
+++ src/main/java/CacheFileEventLogger.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class CacheFileEventLogger extends FileEventLogger {
     private int cacheSize;
-    private List<Event> cache = new ArrayList<Event>();
+    private List<Event> cache = new ArrayList<>();
 
     public CacheFileEventLogger(String fileName, int cacheSize) {
         super(fileName);
